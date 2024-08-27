e31043555aa1c61dca910534974e9e4a95418ad6
Sesiverso <94802700+Sesiverso@users.noreply.github.com>
2024-08-27 15:28:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-document.querySelectorAll('.biome-icon').forEach(biome => {
-    biome.addEventListener('click', (event) => {
-        const parent = event.target.parentElement;
-        parent.style.transform = 'scale(1.5)';
-        parent.style.zIndex = 1000;
-
-        setTimeout(() => {
-            window.location.href = getBiomeLink(parent.id);
-        }, 800);
-    });
-});
-
-function getBiomeLink(id) {
-    switch(id) {
-        case 'pampa':
-            return 'fase1/index.html';
-        case 'mata-atlantica':
-            return 'fase2/index.html';
-        case 'amazonica':
-            return 'fase3/index.html';
-        case 'caatinga':
-            return 'fase4/index.html';
-        case 'pantanal':
-            return 'fase5/index.html';
-        case 'cerrado':
-            return 'fase6/index.html';
-        default:
-            return '#';
-    }
+function mostrarCuriosidade(event, texto) {
+    const curiosidade = document.getElementById('curiosidade');
+    curiosidade.style.left = `${event.pageX}px`;
+    curiosidade.style.top = `${event.pageY - 50}px`;
+    curiosidade.innerText = texto;
+    curiosidade.style.display = 'block';
+    
+    // Remove a curiosidade após 3 segundos
+    setTimeout(() => {
+        curiosidade.style.display = 'none';
+    }, 3000);
 }
